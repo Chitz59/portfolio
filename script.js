de4da2ceb8ae594429e758ba0a1c2f37018985ff
Chhitiz Anand <82027948+Chitz59@users.noreply.github.com>
2023-04-08 15:55:39 +0530

    Updated JS file

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["YouTuber", "Developer", "Designer", "Freelancer"],
+    strings: ["Developer", "Blogger", "Designer", "YouTuber", "Freelancer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
 
   var typed = new Typed(".typing-2", {
-    strings: ["YouTuber", "Developer", "Designer", "Freelancer"],
+    strings: ["Developer", "Blogger", "Designer", "YouTuber", "Freelancer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
